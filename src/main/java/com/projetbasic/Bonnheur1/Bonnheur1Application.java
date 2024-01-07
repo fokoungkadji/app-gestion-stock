7f4d013941d21b47a5d781cc841cc7e7681b307f
bezoutkadji <bezoutkadji@gmail.com>
2024-01-07 15:08:33 +0100

    first commit

---
+++ src/main/java/com/projetbasic/Bonnheur1/Bonnheur1Application.java
@@ -0,0 +1,13 @@
+package com.projetbasic.Bonnheur1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bonnheur1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bonnheur1Application.class, args);
+	}
+
+}
